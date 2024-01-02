b968e31216d1eddc360c76d8cadb8c8d98a99989
ehiosu <ehiandbrothers@gmail.com>
2024-01-02 23:39:17 +0100

    Change App.tsx formating to act as a wrapper component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { FormBuilder } from './components/FormBuilder'
+import { useBuilder } from './hooks/UseBuilder'
+import { Button } from './components/ui/button'
+import { cn } from './lib/utils'
+import { CiPen, CiSettings } from 'react-icons/ci'
+import { Dialog, DialogClose, DialogContent, DialogHeader, DialogTrigger } from './components/ui/dialog'
+import { Separator } from './components/ui/separator'
+import { FormElements } from './lib/types'
+import { SaveButton } from './components/SaveButton'
 
-function App() {
-  const [count, setCount] = useState(0)
+type Props = {}
 
+export const App = (props: Props) => {
+  document.title='LLuvia form Builder'
+  const {currentTab,setCurrentTab,elements,setFormState}=useBuilder()
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+   <main className='w-full flex gap-2 bg-slate-600/80 bg-[url(/graph-paper.svg)]  p-2 flex-col h-screen'>
+    <div className="w-full  bg-[#0c0c0c] p-2 rounded-md flex flex-col">
+      <div className="flex justify-between items-center">
+        <p className='text-[1.6rem] text-white font-bold'>Form Builder</p>
+       <div className="flex items-center gap-2">
+        <Button variant={'outline'} className={cn('dark:text-white text-xs h-8',currentTab=="builder"&&'dark:bg-slate-600',' flex items-center justify-center gap-2')} onClick={()=>{setCurrentTab('builder')}}>Edit Form <CiPen className='text-white text-[0.9rem] mt-[1px]'/></Button>
+        <PreviewForm/>
+        <SaveButton/>
+        <Button className='flex items-center justify-center gap-1 dark:text-white' variant={'outline'} onClick={()=>{setFormState('Settings')}}>Settings <CiSettings className='mt-[1px] text-[0.8rem]'/></Button>
+       <span className='w-6  aspect-square rounded-full inline bg-white '></span>
+       </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+    {
+      currentTab==='builder'&&<FormBuilder/>
+    }
+
+   </main>
   )
 }
 
-export default App
+
+const PreviewForm=()=>{
+  const {elements,currentTab}=useBuilder()
+  return(
+    <Dialog>
+          <DialogTrigger>
+          <Button variant={'outline'} className={cn('dark:text-white text-xs h-8',currentTab=="preview"&&'dark:bg-slate-600')} >Preview</Button>
+          </DialogTrigger>
+          <DialogContent className='w-screen h-screen max-h-screen max-w-full flex flex-col flex-grow gap-0 p-0 bg-[#0c0c0c]  text-white'>
+            <DialogHeader className='dark:text-white text-[1.5rem]  font-semibold px-4'>Form Preview</DialogHeader>
+            <DialogClose className='dark:text-white px-4'/>
+            <Separator className='mt-2 bg-neutral-50/40'/>
+            <p className=' px-4 text-xs text-neutral-100/80 my-2'>Here's a preview of the form you're creating!</p>
+            <div className="flex-1 bg-slate-600 bg-[url(/graph-paper.svg)] p-2">
+              <div className="max-w-[620px] flex flex-col gap-3 flex-grow bg-[#0c0c0c] m-auto h-full  w-full rounded-xl overflow-y-auto p-2">
+                {elements.length>0 &&
+                  elements.map((element)=>{
+                    const PreviewComp = FormElements[element.type].previewCompoennt
+                    return <PreviewComp key={element.id} element={element}/>
+                  })
+                }
+                {
+                  elements.length==0 && <p className="m-auto text-[2rem] text-white font-semibold">No Form Elements</p>
+                }
+              </div>
+            </div>
+          </DialogContent>
+        </Dialog>
+  )
+}
